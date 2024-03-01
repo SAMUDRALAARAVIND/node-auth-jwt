d51dc78614d6e7852afd6838eb78bed4de403f86
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2024-03-01 19:03:06 +0530

    added posts api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const UserSchema = require("./schemas/UserSchema");
 const { generateTokenByUserId, authenticate } = require("./auth");
+const Post = require("./schemas/PostSchema");
 
 const app = express();
 mongoose.connect("mongodb+srv://4byteinteger-root:aravind123@4byteinteger-mongo.fjsr0e4.mongodb.net/accio-users?retryWrites=true&w=majority&appName=4byteinteger-mongo")
@@ -60,6 +61,45 @@ app.post("/login", async (req, resp) => {
 app.get("/users", authenticate, async (req, resp) => {
     const usersList = await UserSchema.find({}, { name: 1, email: 1, city: 1, gender: 1, _id: 0 });
     resp.status(200).json({ result: usersList });
+});
+
+app.get("/search", authenticate, async (req, resp) => {
+    console.log(req.query);
+    if (Object.keys(req.query).length > 0) {
+        const { query } = req.query;
+        const regexp = new RegExp(query, "i");
+        const list = await UserSchema.find({ name: regexp }, { name: 1, email: 1, city: 1, gender: 1, _id: 0 });
+        resp.status(200).json({ result: list });
+    }
+    else resp.status(400).json({ message: "search value is required" })
 })
 
+
+
+app.post('/create', async (req, res) => {
+    try {
+        const { title, content, author } = req.body;
+        const newPost = new Post({
+            title,
+            content,
+            author
+        });
+        await newPost.save();
+
+        res.status(201).json({ id: newPost._id });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+app.get('/posts', async (req, res) => {
+    try {
+        const posts = await Post.find().exec();
+        res.status(200).json(posts);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
 
